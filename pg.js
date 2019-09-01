b46a9f400105aa160fb6b355e3ad4decd87167a9
tuananh15091999 <53033491+tuananh15091999@users.noreply.github.com>
2019-09-01 23:53:56 +0700

    done

---
+++ pg.js
@@ -0,0 +1,13 @@
+const{Client, Pool} = require('pg')
+
+var client = new Client({
+    host: "ec2-50-17-231-192.compute-1.amazonaws.com",
+    database: "dabt65gpdugk2i",
+    port: 5432,
+    password: "895dbc84efd9f3cccbf405cdad37d2dd906aaae8463c6149f9f19f47223d21a5",
+    user: "dheskgvehxorgv"
+})
+
+client.connect();
+
+module.exports = client;
